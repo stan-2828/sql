e3c2227b82c4a7241f9bb91ba327b2889b256750
Stan Wong <stan-demo@users.noreply.github.com>
2025-11-19 21:04:54 -0500

    Added codes from session 5

---
+++ 04_this_cohort/live_code/Cohort_8/module_5/CROSS_JOIN.sql
@@ -0,0 +1,20 @@
+/* MODULE 5 */
+/* CROSS  JOIN */
+
+
+/* 1. CROSS JOIN sizes with product*/
+
+DROP TABLE IF EXISTS TEMP.sizes;
+CREATE TEMP TABLE IF NOT EXISTS TEMP.sizes (size TEXT);
+
+INSERT INTO TEMP.sizes
+VALUES('small'),
+('medium'),
+('large');
+
+SELECT * FROM TEMP.sizes;
+
+SELECT product_name, size
+FROM product
+CROS JOIN temp.sizes
+
